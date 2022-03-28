9976822fd8718f1d094765678db35c91cb22e752
SonOfMagic <1324318532@qq.com>
2022-03-28 21:02:19 +0800

    feat: commit local bundle excalidraw

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import { render } from "react-dom";
-
+// map https://unpkg.com/@excalidraw/excalidraw@0.10.0/dist/excalidraw-assets-dev/vendor-a449655791956f151f51.js
+// @ts-ignore
+// window.EXCALIDRAW_ASSET_PATH = `excalidraw/dist`;
+// http://localhost:3000/excalidraw/dist/excalidraw-assets-dev/vendor-a449655791956f151f51.js
 import App from "./App";
 
 const rootElement = document.getElementById("root");
