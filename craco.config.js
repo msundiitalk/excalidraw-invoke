92fde63488c0b43e7820ce7d92a478807b5b7ad0
msun <msun@diitalk.com>
2022-03-28 22:35:01 -0600

    use Craco instead of config-overrides

--- craco.config.js
+++ craco.config.js
@@ -2,12 +2,9 @@ const path = require("path");
 
 module.exports = {
     devServer: {
-        static: {
-            directory: path.resolve(
-                __dirname,
+        contentBase: path.resolve(__dirname,
                 "./node_modules/@excalidraw/excalidraw"
-            ),
-            publicPath: ["/excalidraw"],
-        },
+        ),
+        contentBasePublicPath: "/excalidraw",
     },
 }
